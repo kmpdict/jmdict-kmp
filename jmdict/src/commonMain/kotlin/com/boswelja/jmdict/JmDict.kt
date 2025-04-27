1a6883782b2f9f1574e195d70561876a62699503
Jack Boswell (boswelja) <boswelja@outlook.com>
2025-04-27 16:12:43 +1000

    Strict deserialize

--- jmdict/src/commonMain/kotlin/com/boswelja/jmdict/JmDict.kt
+++ jmdict/src/commonMain/kotlin/com/boswelja/jmdict/JmDict.kt
@@ -2,18 +2,25 @@ package com.boswelja.jmdict
 
 import jmdict_kmp.jmdict.generated.resources.Res
 import nl.adaptivity.xmlutil.ExperimentalXmlUtilApi
-import nl.adaptivity.xmlutil.XmlBufferedReader
 import nl.adaptivity.xmlutil.core.KtXmlReader
 import nl.adaptivity.xmlutil.serialization.XML
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import java.io.ByteArrayInputStream
 
 internal val Serializer = XML {
+    defaultPolicy {
+        pedantic = true
+        autoPolymorphic = true
+        throwOnRepeatedElement = true
+        isStrictBoolean = true
+        isStrictAttributeNames = true
+        isXmlFloat = true
+        verifyElementOrder = true
+    }
 }
 
 @OptIn(ExperimentalResourceApi::class, ExperimentalXmlUtilApi::class)
 suspend fun openJmDict(): JMdict {
     val input = ByteArrayInputStream(Res.readBytes("files/jmdict.xml"))
-    val bufferedReader = XmlBufferedReader(KtXmlReader(input))
-    return Serializer.decodeFromReader(bufferedReader)
+    return Serializer.decodeFromReader(KtXmlReader(input))
 }
