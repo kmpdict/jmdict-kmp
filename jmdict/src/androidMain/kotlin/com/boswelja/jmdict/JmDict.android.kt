cb3e01b2db75ff4d6e1b00b03be0007cf73b9e02
Jack Boswell (boswelja) <boswelja@outlook.com>
2025-05-24 15:55:54 +1000

    Fix crash on Android

--- jmdict/src/androidMain/kotlin/com/boswelja/jmdict/JmDict.android.kt
+++ jmdict/src/androidMain/kotlin/com/boswelja/jmdict/JmDict.android.kt
@@ -6,8 +6,6 @@ import java.util.zip.GZIPInputStream
 actual class JmDictReader(private val context: Context) {
     actual suspend fun streamJmDict(): Sequence<Entry> {
         val reader = GZIPInputStream(context.resources.openRawResource(R.raw.jmdict)).bufferedReader()
-        return reader.useLines { linesSequence ->
-            linesSequence.asEntrySequence()
-        }
+        return reader.lineSequence().asEntrySequence()
     }
 }
