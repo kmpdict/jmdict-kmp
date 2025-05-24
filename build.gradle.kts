65605f4c563ae747c3d02b1d5517b0fa39ac5a1a
Jack Boswell <boswelja@outlook.com>
2025-05-24 18:19:20 +1000

    Detekt merge fixes (#3)
    * Generator detekt config
    
    * Try to fix Detekt report generation
    
    * Try to fix area-labeler
    
    * Update detektMerge
    
    * detektMerge as separate step

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,15 +8,8 @@ plugins {
 
 val detektMerge by tasks.registering(io.gitlab.arturbosch.detekt.report.ReportMergeTask::class) {
     output.set(rootProject.layout.buildDirectory.file("reports/detekt/merge.sarif"))
-}
-
-// TODO this is not good, it increases configuration time
-subprojects {
-    tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
-        finalizedBy(detektMerge)
+    val reportTree = fileTree(baseDir = rootDir) {
+        include("**/detekt/main.sarif")
     }
-
-    detektMerge {
-        input.from(tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().map { it.sarifReportFile })
-    }
+    input.from(reportTree)
 }
