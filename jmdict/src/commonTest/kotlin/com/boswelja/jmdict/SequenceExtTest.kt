d077cfe23f48a352fb6b08714d898eb935418e25
Jack Boswell (boswelja) <boswelja@outlook.com>
2025-05-24 16:36:39 +1000

    Create some tests for chunkedUntil

---
+++ jmdict/src/commonTest/kotlin/com/boswelja/jmdict/SequenceExtTest.kt
@@ -0,0 +1,35 @@
+package com.boswelja.jmdict
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class SequenceExtTest {
+
+    @Test
+    fun `chunkedUntil with empty sequence`() {
+        val emptySequence = emptySequence<String>()
+        val chunkedUntil = emptySequence.chunkedUntil { it == "Hello, world!" }
+        assertTrue(chunkedUntil.none())
+    }
+
+    @Test
+    fun `chunkedUntil where no elements match predicate`() {
+        val targetSequence = sequenceOf(
+            "Hello,",
+            "World!",
+            "I'm",
+            "a",
+            "sequence!"
+        )
+        val chunkedUntil = targetSequence.chunkedUntil { it.endsWith("~") } // Nothing in targetSequence ends with ~
+        assertEquals(
+            1,
+            chunkedUntil.count()
+        )
+        assertEquals(
+            targetSequence.toList(),
+            chunkedUntil.first()
+        )
+    }
+}
