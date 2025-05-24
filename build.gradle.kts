73158ec954fce171fba934f6f9092907a020052e
Jack Boswell (boswelja) <boswelja@outlook.com>
2025-05-24 17:59:37 +1000

    Update detektMerge

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,8 @@ plugins {
 
 val detektMerge by tasks.registering(io.gitlab.arturbosch.detekt.report.ReportMergeTask::class) {
     output.set(rootProject.layout.buildDirectory.file("reports/detekt/merge.sarif"))
-    val reportTree = fileTree(baseDir = rootDir).include("**/detekt/main.sarif")
+    val reportTree = fileTree(baseDir = rootDir) {
+        include("**/detekt/main.sarif")
+    }
     input.from(reportTree)
 }
