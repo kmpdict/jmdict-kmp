1208435151951c86ef8ba4b7434c8715393142d2
Jack Boswell (boswelja) <boswelja@outlook.com>
2025-05-24 16:53:31 +1000

    Ignore silly tests

--- jmdict/src/androidInstrumentedTest/kotlin/com/boswelja/jmdict/JmDictReaderTest.kt
+++ jmdict/src/androidInstrumentedTest/kotlin/com/boswelja/jmdict/JmDictReaderTest.kt
@@ -2,11 +2,13 @@ package com.boswelja.jmdict
 
 import androidx.test.core.app.ApplicationProvider
 import kotlinx.coroutines.test.runTest
+import kotlin.test.Ignore
 import kotlin.test.Test
 
 class JmDictReaderTest {
 
     @Test
+    @Ignore("This is a placeholder test that we can use to verify simply opening the stream.")
     fun streamJmDict() = runTest {
         val reader = JmDictReader(ApplicationProvider.getApplicationContext())
         var entryCount = 0
--- jmdict/src/jvmTest/kotlin/com/boswelja/jmdict/JmDictReaderTest.kt
+++ jmdict/src/jvmTest/kotlin/com/boswelja/jmdict/JmDictReaderTest.kt
@@ -1,11 +1,13 @@
 package com.boswelja.jmdict
 
 import kotlinx.coroutines.test.runTest
+import kotlin.test.Ignore
 import kotlin.test.Test
 
 class JmDictReaderTest {
 
     @Test
+    @Ignore("This is a placeholder test that we can use to verify simply opening the stream.")
     fun streamJmDict() = runTest {
         val reader = JmDictReader()
         reader.streamJmDict()
