36cfa17e3c548ff69733ded145d00a3a32684839
Jack Boswell <boswelja@outlook.com>
2025-05-25 13:33:25 +1000

    Publishing setup (#5)
    * Prep for publishing
    
    * Versions
    
    * Publish workflow
    
    * Try fix labeler
    
    * Update workflows
    
    * Fix ci
    
    * Update publish
    
    * Calculate date-based version
    
    * Disable OSS pub for now

--- buildSrc/src/main/kotlin/com.boswelja.publish.gradle.kts
+++ buildSrc/src/main/kotlin/com.boswelja.publish.gradle.kts
@@ -1,6 +1,7 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
-    id("maven-publish")
-    id("signing")
+    id("com.vanniktech.maven.publish")
 }
 
 interface CustomPublishingExtension {
@@ -14,74 +15,43 @@ val extension = project.extensions.create<CustomPublishingExtension>("publish")
 group = findProperty("group")!!
 version = findProperty("version")!!
 
-signing {
-    val signingKey: String? by project
-    val signingPassword: String? by project
-    useInMemoryPgpKeys(signingKey, signingPassword)
-    sign(publishing.publications)
-}
-
 afterEvaluate {
-    val javadocJar by tasks.registering(Jar::class) {
-        archiveClassifier = "javadoc"
-    }
-
     publishing {
         repositories {
-            val githubUsername = findProperty("githubUsername")?.toString()
-            val githubToken = findProperty("githubToken")?.toString()
-            if (githubUsername != null && githubToken != null) {
-                maven("https://maven.pkg.github.com/boswelja/kotlin-datatypes") {
-                    name = "github"
-                    credentials {
-                        username = githubUsername
-                        password = githubToken
-                    }
-                }
-            }
-            val ossrhUsername = findProperty("ossrhUsername")?.toString()
-            val ossrhPassword = findProperty("ossrhPassword")?.toString()
-            if (ossrhUsername != null && ossrhPassword != null) {
-                maven("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/") {
-                    name = "oss"
-                    credentials {
-                        username = ossrhUsername
-                        password = ossrhPassword
-                    }
-                }
-            }
-        }
-
-        publications.withType<MavenPublication> {
-            artifact(javadocJar)
-            pom {
-                name = project.name
-                description = extension.description
-                url = extension.repositoryUrl.get()
-                licenses {
-                    license {
-                        name = extension.license.get()
-                        url = "${extension.repositoryUrl}/blob/main/LICENSE"
-                    }
-                }
-                developers {
-                    developer {
-                        id = "boswelja"
-                        name = "Jack Boswell (boswelja)"
-                        email = "boswelja@outlook.com"
-                        url = "https://github.com/boswelja"
-                    }
-                }
-                scm {
-                    url.set(extension.repositoryUrl.get())
-                }
+            maven("https://maven.pkg.github.com/boswelja/jmdict-kmp") {
+                name = "github"
+                credentials(PasswordCredentials::class)
             }
         }
     }
 
-    // TODO: Remove after https://youtrack.jetbrains.com/issue/KT-46466 is fixed
-    //  Thanks to KSoup repository for this code snippet
-    tasks.withType(AbstractPublishToMaven::class).configureEach {
-        dependsOn(tasks.withType(Sign::class))
+    mavenPublishing {
+        coordinates(group as String, name, version as String)
+
+        publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
+        signAllPublications()
+
+        pom {
+            name = project.name
+            description = extension.description
+            url = extension.repositoryUrl.get()
+            licenses {
+                license {
+                    name = extension.license.get()
+                    url = "${extension.repositoryUrl}/blob/main/LICENSE"
+                }
+            }
+            developers {
+                developer {
+                    id = "boswelja"
+                    name = "Jack Boswell (boswelja)"
+                    email = "boswelja@outlook.com"
+                    url = "https://github.com/boswelja"
+                }
+            }
+            scm {
+                url.set(extension.repositoryUrl.get())
+            }
+        }
     }
 }
